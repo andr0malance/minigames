4fb4129fa50ac7ae7f988940f8d696cd03f6a082
andr0malance <147593308+andr0malance@users.noreply.github.com>
2025-05-27 16:45:41 +0300

    Add files via upload

---
+++ minigames/Form2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace minigames
+{
+    public partial class NicknameForm : Form
+    {
+        public NicknameForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
